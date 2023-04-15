5d2f8b737bd6e55a2fb0a3dc8362d0e895babfd5
Pankaj <89864509+0410Pankajv@users.noreply.github.com>
2023-04-16 02:29:56 +0530

    Sol 3

---
+++ Sorting and Searching/ferriswheel.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define sp fixed<<setprecision
+#define ld long double
+#define ll long long
+#define PI acos(-1)
+#define all(x) (x).begin(), (x).end()
+#define fr(a,b) for(ll i = a; i < b; i++)
+#define rep(i,a,b) for(ll i = a; i < b; i++)
+
+ll mod=1e9+7;
+ll power(ll a,ll b)
+{
+ ll ans=1ll;
+   while(b>0)
+  {
+       if(b&1)
+        ans=(ans*a);
+      b=b>>1;
+     a=(a*a);
+  }
+return ans;
+}
+
+ll powerm(ll a,ll b)
+{
+ ll ans=1ll;
+   while(b>0)
+  {
+       if(b&1)
+        ans=(ans*a)%mod;
+      b=b>>1;
+     a=(a*a)%mod;
+  }
+return ans;
+}
+
+
+int main()
+{
+//freopen("input.txt", "r", stdin); // redirects standard input
+//	freopen("output.txt", "w", stdout); // redirects standard output
+FAST
+ll tc=1;
+//cin>>tc;
+while(tc--)
+{
+    ll n,x;
+    cin>>n>>x;
+    vector<ll> p(n);
+    fr(0,n)cin>>p[i];
+    sort(p.begin(),p.end());
+    ll ans=0,i=0,j=n-1;
+    while(i<j){
+        if(p[i]+p[j]<=x){
+            ans++;
+            i++;
+            j--;
+
+
+        }else{
+            j--;
+        }
+
+
+    }
+    ans+=n-ans*2;
+
+
+
+
+
+
+
+
+cout<<ans<<"\n";
+
+
+}
+
+
+
+
+
+}
